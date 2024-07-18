68ade5c05f1a1521a124bb44eb9ea8abaab498e3
AruaSunday <aruasamuel08141@gmail.com>
2024-07-18 23:12:34 +0100

    counter

---
+++ script.js
@@ -0,0 +1,40 @@
+let btnClick = document.querySelector(".cli");
+let resClick = document.querySelector(".res")
+let btnnClick = document.querySelector(".cl");
+let counter = document.querySelector("h1");
+
+let count = 0;
+
+btnClick.addEventListener('click', () => {
+myFunction2(myFunction1);
+});
+btnnClick.addEventListener('click', () => {
+myFunction(myFunction3);
+});
+resClick.addEventListener('click', () => {
+myFunctions(myFunction_);
+});
+
+function myFunction1(){
+    return count+= 10;
+}
+function myFunction3(){
+    if (count === 0){
+        return count = 0;
+    }
+    else{
+        return count-= 10;
+    }
+}
+function myFunction_(){
+return count = 0;
+};
+function myFunction2(paramFunc){
+counter.innerText = paramFunc();
+}
+function myFunction(paramFun){
+counter.innerText = paramFun();
+}
+function myFunctions(paramFuns){
+counter.innerText = paramFuns();
+}
